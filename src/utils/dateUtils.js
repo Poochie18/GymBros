8611f8464a08582443b7828bbf59cd67bc0cb785
3030cocacola@gmail.com <3030cocacola@gmail.com>
2025-08-08 23:02:21 +0300

    yo

--- src/utils/dateUtils.js
+++
@@ -1,7 +0,0 @@
-export function isoDate(d){
-  const yyyy = d.getFullYear()
-  const mm = String(d.getMonth()+1).padStart(2,'0')
-  const dd = String(d.getDate()).padStart(2,'0')
-  return `${yyyy}-${mm}-${dd}`
-}
-export function todayIso(){ return isoDate(new Date()) }
